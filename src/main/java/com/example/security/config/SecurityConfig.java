f2baa6d0b6881034309384a10791b3ac7130d18e
jaejoon <dkansk924@naver.com>
2021-05-05 21:52:47 +0900

    [add] JWT 적용을위한 SecurityConfig 설정변경.

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,19 +1,52 @@
 package com.example.security.config;
 
+import com.example.security.auth.CustomAuthenticationEntryPoint;
+import com.example.security.auth.JwtAuthenticationFilter;
+import com.example.security.auth.JwtAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthenticationProvider authenticationProvider;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    private final static String[] PUBLIC_URL = {"/member", "/login"};
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/member")
-            .permitAll().anyRequest().authenticated();
-        http.formLogin();
-        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers(PUBLIC_URL).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(customAuthenticationEntryPoint);
+
+        http.csrf().disable()
+            .httpBasic().disable()
+            .formLogin().disable()
+            .logout().disable();
+
+        http.addFilterBefore(new JwtAuthenticationFilter(authenticationManagerBean()),
+            UsernamePasswordAuthenticationFilter.class);
+
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
     }
 }
