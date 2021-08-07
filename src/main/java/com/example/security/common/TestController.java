26ec2d6673dd6f9392ba852de18541dc101938f1
jaejoon <dkansk924@naver.com>
2021-08-07 21:21:29 +0900

    cookie 설정 확인

--- src/main/java/com/example/security/common/TestController.java
+++ src/main/java/com/example/security/common/TestController.java
@@ -1,5 +1,7 @@
 package com.example.security.common;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,19 @@ public class TestController {
     @GetMapping("/session")
     public String setSession(HttpSession httpSession){
         httpSession.setAttribute("user","kjj");
+        return "ok";
+    }
+
+
+    // Cookie 의 age 값을 설정하지않고 사용하게되면 브라우저가 종료될때까지만 유지하게된다.
+    // Session 으로 생성되는 Cookie 같은경우가 그렇다.
+    // 또한 HttpOnly 속성은 ture 로 설정된다.
+    @GetMapping("/cookieAge")
+    public String checkCookieLifeCycle(HttpServletResponse httpServletResponse){
+        Cookie cookie = new Cookie("testCookie","TestValue");
+        cookie.setMaxAge(60*30); // 단위는 seconds 임
+        cookie.setHttpOnly(true);
+        httpServletResponse.addCookie(cookie);
         return "ok";
     }
 }
